48916d008535d1d1cb1d9c89a83f56a306c49f8b
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2019-12-30 11:25:34 -0300

    finished 10.2

--- 10_generics_traits_lifetimes/02_traits/src/main.rs
+++ 10_generics_traits_lifetimes/02_traits/src/main.rs
@@ -1,3 +1,66 @@
+pub trait Summary {
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify_1<T: Summary>(item: T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// syntax sugar for the notify_1
+pub fn notify_2(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best
+    hockey team in the NHL."),
+    };
+
+    notify_1(tweet);
+    notify_2(article)
 }
