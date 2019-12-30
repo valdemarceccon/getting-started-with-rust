111723be4ce5d112748cfe19170ee0fe5008d406
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2019-12-30 10:47:41 -0300

    finished section 10.1

--- 10_generics_traits_lifetimes/01_generic_data_types/src/main.rs
+++ 10_generics_traits_lifetimes/01_generic_data_types/src/main.rs
@@ -1,25 +1,48 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+// Do not work
+//fn largest<T>(list: &[T]) -> T {
+//    let mut largest = list[0];
+//
+//    for &item in list.iter() {
+//        if item > largest {
+//            largest = item;
+//        }
+//    }
+//
+//    largest
+//}
+
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+//    let number_list = vec![34, 50, 25, 100, 65];
+//
+//    let result = largest(&number_list);
+//
+//    println!("The largest number is {}!", result);
+//
+//    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+//
+//    let result = largest(&number_list);
+//
+//    println!("The largest number is {}!", result);
 
-    let result = largest(&number_list);
+    let p = Point{x:5, y:6};
+    println!("X value: {}\nY value: {}", p.x(), p.y);
 
-    println!("The largest number is {}!", result);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-
-    println!("The largest number is {}!", result);
+    let p = Point{x: 5.0, y: 6.0};
+    println!("Distance from origin: {}", p.distance_from_origin());
 }
