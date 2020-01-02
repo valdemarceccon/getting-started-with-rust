09f7edeb3c25bd134583527fbf20a423c3e48d15
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2020-01-02 11:24:57 -0300

    finishing 11.1

--- 11_automated_tests/01_how_to_write_tests/src/lib.rs
+++ 11_automated_tests/01_how_to_write_tests/src/lib.rs
@@ -23,6 +23,46 @@ mod tests {
         assert_eq!(4, add_two(2));
     }
 
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was {}", result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got 200.")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal to four?"))
+        }
+    }
+
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new (value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value)
+        }
+
+        Guess {
+            value
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -39,4 +79,8 @@ impl Rectangle {
 
 fn add_two(a: i32) -> i32 {
     a + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello, {}", name)
 }
